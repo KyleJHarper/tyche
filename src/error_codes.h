27e729de03d447ecdb6db1ac55a11379fa1a3abe
KyleJHarper <KyleJHarper@gmail.com>
2015-07-16 00:04:02 -0500

    Forgot to update the error codes with descriptions.

--- src/error_codes.h
+++ src/error_codes.h
@@ -16,10 +16,10 @@
 const int E_FAILURE_THRESHOLD  = 100;
 const int E_WARNING_THRESHOLD  = 200;
 
-const int E_OK                 =   0;
-const int E_GENERIC            =   1;
-const int E_TRY_AGAIN          = 101;
-const int E_BUFFER_POOFED      = 121;
+const int E_OK                 =   0;  // All good.
+const int E_GENERIC            =   1;  // General errors which don't specifically mean any regular condition/error.
+const int E_TRY_AGAIN          = 101;  // Conditions may exist (e.g. threading) requiring a caller the intended action again.
+const int E_BUFFER_POOFED      = 121;  // Some operations cause buffers to *poof* as we swap them around.  Handle appropriately.
 
 
 #endif /* SRC_ERROR_CODES_H_ */
